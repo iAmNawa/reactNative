a7c32ac57b3b7db1d406dbe92a2fb05b96b05807
Paul Borawski <paulborawski14@gmail.com>
2019-01-21 12:43:45 -0800

    album cover being rendered

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,12 +4,13 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
+  const { title, artist, thumbnail_image, image } = album;
   const {
     thumbnailStyle,
     headerContentStyle,
     thumbnailContainerStyle,
-    headerTextStyle
+    headerTextStyle,
+    imageStyle
   } = styles;
 
   return (
@@ -22,6 +23,9 @@ const AlbumDetail = ({ album }) => {
           <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle} source={{ uri: image }} />
       </CardSection>
     </Card>
   );
@@ -44,6 +48,11 @@ const styles = {
     alignItems: 'center',
     marginLeft: 10,
     marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
